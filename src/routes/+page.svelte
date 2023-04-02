daa3e84f26789a7e08b7e6ab4219c2d2abb29046
MoulinLouis <moulin.louis93@gmail.com>
2023-04-02 15:03:26 +0200

    add: opengraph meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,24 @@
 <svelte:head>
   <title>Tesla - Green IT</title>
   <link rel="image_src" href="/images/home/hero-model-3.jpg" />
-  <meta name="description" content="Tesla is accelerating the world&#039;s transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses." />
+  <meta name="description" content="Tesla is accelerating the world';s transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses." />
   <meta name="keywords" content="Tesla, Model S, Model Y, Model X, Model 3, Énergie" />
   <link rel="canonical" href="https://green-it.vercel.app/" />
 
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:site" content="@tesla" />
-  <meta name="twitter:description" content="Tesla is accelerating the world&#039;s transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses." />
+  <meta name="twitter:description" content="Tesla is accelerating the world's transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses." />
   <meta name="twitter:title" content="Tesla - Green IT" />
   <meta name="twitter:creator" content="@tesla" />
   <meta name="twitter:url" content="https://green-it.vercel.app/" />
+
+  <meta property="og:title" content="Tesla - Green IT" />
+  <meta property="og:description" content="Tesla is accelerating the world's transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses." />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://green-it.vercel.app/" />
+  <meta property="og:image" content="/images/home/hero-model-3.jpg" />
+  <meta property="og:site_name" content="Tesla - Green IT" />
+  <meta property="og:locale" content="en_US" />
 </svelte:head>
 
 <main class="flex flex-col w-full h-full">
