e95317b6c7327a2f0d1f7ea6238a75e3e2dbc8e1
Alexis LOURS <alexislours@protonmail.com>
2023-04-12 23:59:12 +0200

    chore: compress JS helpers

--- src/components/JSHelpers.svelte
+++ src/components/JSHelpers.svelte
@@ -1,38 +1,5 @@
 <svelte:head>
   <script>
-    document.addEventListener("DOMContentLoaded", () => {
-      const images = document.querySelectorAll("img");
-      images.forEach((img) => {
-        img.addEventListener("load", () => {
-          img.classList.remove("opacity-0");
-        });
-
-        if (img.complete) {
-          img.classList.remove("opacity-0");
-        }
-      });
-      function handleIntersection(entries, observer) {
-        entries.forEach(async (entry) => {
-          if (entry.isIntersecting) {
-            await new Promise((resolve) => setTimeout(resolve, 500));
-            entry.target.classList.remove("opacity-0");
-            entry.target.classList.add("animate-fly-in");
-          } else {
-            entry.target.classList.remove("animate-fly-in");
-            entry.target.classList.add("opacity-0");
-          }
-        });
-      }
-
-      const observer = new IntersectionObserver(handleIntersection);
-      const flyInElements = document.querySelectorAll(".fly-in");
-      flyInElements.forEach((element) => {
-        element.classList.add("opacity-0");
-      });
-
-      flyInElements.forEach((element) => {
-        observer.observe(element);
-      });
-    });
+  document.addEventListener("DOMContentLoaded",(()=>{document.querySelectorAll("img").forEach((e=>{e.addEventListener("load",(()=>{e.classList.remove("opacity-0")})),e.complete&&e.classList.remove("opacity-0")}));const e=new IntersectionObserver((function(e,t){e.forEach((async e=>{e.isIntersecting?(await new Promise((e=>setTimeout(e,500))),e.target.classList.remove("opacity-0"),e.target.classList.add("animate-fly-in")):(e.target.classList.remove("animate-fly-in"),e.target.classList.add("opacity-0"))}))})),t=document.querySelectorAll(".fly-in");t.forEach((e=>{e.classList.add("opacity-0")})),t.forEach((t=>{e.observe(t)}))}));
   </script>
 </svelte:head>
