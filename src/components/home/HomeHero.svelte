0520f90a76f753b063b1f541aee3d88bed65a6f6
Alexis LOURS <alexislours@protonmail.com>
2023-04-12 23:43:29 +0200

    feat: remove eager loading

--- src/components/home/HomeHero.svelte
+++ src/components/home/HomeHero.svelte
@@ -38,7 +38,7 @@
       type="image/jpeg" />
     <img
       src="/images/home/hero-model-{l}.jpg"
-      loading="{i === 0 ? 'eager' : 'lazy'}"
+      loading="{i === 0 ? undefined : 'lazy'}"
       class="h-full w-screen object-cover opacity-0 transition-all duration-1000"
       alt="Electric car model {l} from Tesla" />
   </picture>
