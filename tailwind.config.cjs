009d6d22bf6106ac2bbee979741f1cc34debdb56
Alexis LOURS <alexislours@protonmail.com>
2023-04-12 21:11:58 +0200

    feat: optimize webfonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,6 @@ module.exports = {
       // use the font from /fonts/gotthamssm.woff2
       fontFamily: {
         medium: ["GothamSSM", "sans-serif"],
-        sans: ["GothamSSM-Light", "sans-serif"],
         big: ["GothamSSM-Big", "sans-serif"],
         huge: ["GothamSSM-Large", "sans-serif"],
       },
