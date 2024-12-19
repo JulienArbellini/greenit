0e89590df9ea2c4168aef51ac001adf5464602b0
Julien Arbellini <arbellini.j@gmail.com>
2024-12-19 13:25:15 +0100

    dev

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -1474,7 +1474,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1drsqhj"
+  version_hash: "1ilmb3n"
 };
 async function get_hooks() {
   let handle;
